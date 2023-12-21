bbd60daa805ce08088bac0a35e52cdba1f66463f
GHARBIMouna <mouna.gharbi@centralesupelec.fr>
2023-12-21 10:16:51 +0100

    add csv of real MS spectra

---
+++ Unrolled_methods/U_ISTA/ISTA_joint_architecture.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch
+
+Soft = nn.Softplus()
+R=nn.ReLU()
+class stepsize_regularization_arch(torch.nn.Module):
+    def __init__(self):
+        super(stepsize_regularization_arch, self).__init__()
+        self.gamma=nn.Parameter(torch.DoubleTensor([0.2]).cuda(), requires_grad=True)
+        self.xsi=nn.Parameter(torch.DoubleTensor([4]).cuda(), requires_grad=True)
+        
+    def forward(self):
+        gamma=R(self.gamma)
+        xsi=R(self.xsi)
+        return (gamma,xsi)
